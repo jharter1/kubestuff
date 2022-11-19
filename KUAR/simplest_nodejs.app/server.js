82e95e9d4b92681d1905c9ee7373d08b2b9cc29e
Jack Harter <jack.harter@me.com>
2022-11-19 11:29:09 -0600

    cleanup

--- KUAR/simplest_nodejs.app/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello all ye who enter!');
-});
-app.listen(3000, function () { 
-    console.log('Gehoren auf port 3000!'); 
-    console.log(' http://localhost:3000');
-});
